fd6c66fbda77075b25a02d0a498fafb43fe79dd8
iakuliaev <iakuliaev@gmail.com>
2022-03-25 01:28:39 +0300

    feat: add Dichotomy method

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import openpyxl
 
 from methods.fibonacci import fibonacci_search
 from methods.golden_ratio import golden_ratio_search
+from methods.dichotomy import dichotomy_search
 
 
 def function(x):
@@ -20,7 +21,12 @@ fib = fibonacci_search(function, 1, 7, 0.01, 20)
 df_fib = pd.DataFrame(data=fib[2], columns=['left_border', 'right_border', 'a', 'b', 'f(a)', 'f(b)'])
 print(df_fib)
 
+print('--------------Dichotomy-Search-----------------------')
+dich = dichotomy_search(function, 1, 7, 0.01)
+df_dich = pd.DataFrame(data=dich[2], columns=['left_border', 'right_border', 'middle', 'f(middle)'])
+print(df_dich)
+
 with pd.ExcelWriter("AppliedMathsTest.xlsx") as writer:
     df_gr.to_excel(writer, sheet_name='GoldenRatio')
     df_fib.to_excel(writer, sheet_name='Fibonacci')
-
+    df_dich.to_excel(writer, sheet_name='Dichotomy')
---
+++ methods/dichotomy.py
@@ -0,0 +1,35 @@
+import math
+import numpy as np
+
+
+def f(x):
+    return math.sin(x) - math.log(pow(x, 2)) - 1
+
+
+def dichotomy_search(func, left_border: float, right_border: float, precision: float):
+
+    middlex = (left_border + right_border) / 2
+    left = func(left_border)
+    right = func(right_border)
+    i = 0
+    iters_data = np.array([left_border, right_border, middlex, func(middlex)])
+    while abs(right_border - left_border) > precision:
+        i+=1
+        if left > right:
+            if func(middlex) < left:
+                left_border = middlex
+                left = func(left_border)
+                middlex = (left_border + right_border) / 2
+        else:
+            if func(middlex) < right:
+                right_border = middlex
+                right = func(right_border)
+                middlex = (left_border + right_border) / 2
+
+        iters_data = np.append(iters_data, [left_border, right_border, middlex, func(middlex)])
+
+    return middlex, func(middlex), iters_data.reshape((iters_data.size // 4, 4))
+
+
+print("-------------------DICHOTOMY------------------")
+print("the end")
