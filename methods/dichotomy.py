358dca739eb9c746bce26f0095b99ec51450cbfe
De-Bill <podshivalov.o29@gmail.com>
2022-03-25 11:24:01 +0300

    fix: decrease number of calculating func(middle)

--- methods/dichotomy.py
+++ methods/dichotomy.py
@@ -8,17 +8,18 @@ def dichotomy_search(func, left_border: float, right_border: float, precision: f
     f_right = func(right_border)
     iters_data = np.array([left_border, right_border, middle_x, func(middle_x)])
     while abs(right_border - left_border) > precision:
+        f_middle = func(middle_x)
         if f_left > f_right:
-            if func(middle_x) < f_left:
+            if f_middle < f_left:
                 left_border = middle_x
-                f_left = func(left_border)
+                f_left = f_middle
                 middle_x = (left_border + right_border) / 2
         else:
-            if func(middle_x) < f_right:
+            if f_middle < f_right:
                 right_border = middle_x
-                f_right = func(right_border)
+                f_right = f_middle
                 middle_x = (left_border + right_border) / 2
 
-        iters_data = np.append(iters_data, [left_border, right_border, middle_x, func(middle_x)])
+        iters_data = np.append(iters_data, [left_border, right_border, middle_x, f_middle])
 
     return middle_x, func(middle_x), iters_data.reshape((iters_data.size // 4, 4))
