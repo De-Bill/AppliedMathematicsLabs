3a7bcb71e3f9d33042c77e0ebcfda7ef05c0a5f8
De-Bill <podshivalov.o29@gmail.com>
2022-03-25 11:34:11 +0300

    fix: refactor if-else statements

--- methods/dichotomy.py
+++ methods/dichotomy.py
@@ -9,16 +9,14 @@ def dichotomy_search(func, left_border: float, right_border: float, precision: f
     iters_data = np.array([left_border, right_border, middle_x, func(middle_x)])
     while abs(right_border - left_border) > precision:
         f_middle = func(middle_x)
-        if f_left > f_right:
-            if f_middle < f_left:
-                left_border = middle_x
-                f_left = f_middle
-                middle_x = (left_border + right_border) / 2
-        else:
-            if f_middle < f_right:
-                right_border = middle_x
-                f_right = f_middle
-                middle_x = (left_border + right_border) / 2
+        if f_left > f_right and f_middle < f_left:
+            left_border = middle_x
+            f_left = f_middle
+            middle_x = (left_border + right_border) / 2
+        elif f_left < f_right and f_middle < f_right:
+            right_border = middle_x
+            f_right = f_middle
+            middle_x = (left_border + right_border) / 2
 
         iters_data = np.append(iters_data, [left_border, right_border, middle_x, f_middle])
 
