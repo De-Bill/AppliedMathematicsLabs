75fe6cfc4236b5b918ab188ebd614b5898753822
De-Bill <podshivalov.o29@gmail.com>
2022-04-16 19:43:37 +0300

    refactor: modify project structure

--- lab-1/main.py
+++ lab-1/main.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import openpyxl
 
 from methods.fibonacci import fibonacci_search
 from methods.golden_ratio import golden_ratio_search
@@ -32,7 +31,7 @@ par = parabola_search(function, 1, 7, 0.01)
 df_par = pd.DataFrame(data=par[2], columns=['left_border', 'right_border', 'middle', 'f(middle)'])
 print(df_par)
 
-with pd.ExcelWriter("AppliedMathsTest.xlsx") as writer:
+with pd.ExcelWriter("../AppliedMathsTest.xlsx") as writer:
     df_dich.to_excel(writer, sheet_name='Dichotomy')
     df_gr.to_excel(writer, sheet_name='GoldenRatio')
     df_fib.to_excel(writer, sheet_name='Fibonacci')
