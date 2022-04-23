40214db875c0c7ccae8043ab627d1dc9eb8decac
De-Bill <podshivalov.o29@gmail.com>
2022-04-23 12:55:17 +0300

    feat: add correct fractional step

--- lab2/learning_rate_variations/fractional.py
+++ lab2/learning_rate_variations/fractional.py
@@ -3,19 +3,30 @@ import numpy as np
 import lab2.given_func as given
 
 
-def grad_desc_with_fractional_step(x_start, y_start, learning_rate, n_iterations, epsilon, delta):
+def grad_desc_with_fractional_step(x_start, y_start, learning_rate, precision, epsilon, delta):
+    iteration = 0
+    print(
+        f"iteration: {iteration:.3f}    x:, {x_start:.3f}    y: {y_start:.3f}    f(x,y): {given.f(x_start, y_start):.5f}")
+    iteration += 1
+
     points = np.array([x_start, y_start])
-    for i in range(n_iterations):
-        x_next = x_start - learning_rate * given.df_x(x_start, y_start)
-        y_next = y_start - learning_rate * given.df_y(x_start, y_start)
-        if given.f(x_start, y_start) - given.f(x_next, y_next) > epsilon * learning_rate * (given.df_x(x_start) ** 2):
-            x_next = delta * x_start
-            y_next = delta * x_start
+    while True:
+        dx, dy = given.df_x(x_start, y_start), given.df_y(x_start, y_start)
+        x_next = x_start - learning_rate * dx
+        y_next = y_start - learning_rate * dy
+
+        if given.f(x_start, y_start) - given.f(x_next, y_next) > epsilon * learning_rate * (dx ** 2 + dy ** 2):
+            x_next = x_start - (learning_rate * delta) * dx
+            y_next = y_start - (learning_rate * delta) * dy
 
         x_start = x_next
         y_start = y_next
         points = np.append(points, [x_start, y_start])
 
-        print(f"iteration: {i:.3f}    x:, {x_start:.3f}    y: {y_start:.3f}    f(x,y): {given.f(x_start, y_start):.5f}")
+        print(f"iteration: {iteration:.3f}    x:, {x_start:.3f}    y: {y_start:.3f}    f(x,y): {given.f(x_start, y_start):.5f}")
+        iteration += 1
+
+        if np.abs(given.df_x(x_start, y_start)) <= precision and np.abs(given.df_y(x_start, y_start)) <= precision:
+            break
 
     return points
--- lab2/test.py
+++ lab2/test.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import given_func as given
 from learning_rate_variations.const import grad_desc_with_const_lr
 from learning_rate_variations.fractional import grad_desc_with_fractional_step
-from learning_rate_variations.fibonacci import grad_desc_fibonacci_step
+from learning_rate_variations.fibonacci import grad_desc_with_fibonacci_step
 from learning_rate_variations.golden_ratio import grad_desc_with_golden_ratio_step
 
 x = np.linspace(-1.8, 1.8, 50)
@@ -13,7 +13,8 @@ X, Y = np.meshgrid(x, y)
 F = given.f(X, Y)
 
 x_start, y_start = 1, 1
-points = grad_desc_fibonacci_step(x_start, y_start, 0.05, 10)
+precision = 0.06
+points = grad_desc_with_fractional_step(x_start, y_start, 0.2, precision, 0.1, 0.95)
 
 ax = plt.subplots()[1]
 for i in range(0, points.size, 2):
