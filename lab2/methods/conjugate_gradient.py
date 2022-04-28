95bec79ed32bdc4d080fca9fb7d2363169d93e9c
iakuliaev <iakuliaev@gmail.com>
2022-04-28 14:10:44 +0300

    feat: add conjugate grad

---
+++ lab2/methods/conjugate_gradient.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+
+def f(x, y):  # given func
+    return x * x + 2 * y * y + 2 * x * y
+
+
+def calculate_step_f(x_coeff, y_coeff):
+    return (x_coeff + y_coeff) / \
+           (4 * x_coeff + 6 * y_coeff)
+
+
+def df_x(x, y):  # derivative of the given func -- df/dx
+    return 2 * x + 2 * y
+
+
+def df_y(x, y):  # -- df/dy
+    return 4 * y + 2 * x
+
+
+def grad(x):
+    return np.array([df_x(x[0], x[1]), df_y(x[0], x[1])])
+
+
+def norm(x):  # vector_length
+    return np.sqrt(np.power(x[0], 2) + np.power(x[1], 2))
+
+
+def find_step(x_prev, func):
+    return func(x_prev[0], x_prev[1])
+
+
+def conjugate_gradient(epsilon):
+    x_prev = np.array([-50, -50], dtype='float64')
+    prev_gradient = grad(x_prev)
+    k = 0
+    x_k = np.array([-50, -50], dtype='float64')
+    while True:
+        prev_gradient = grad(x_prev)
+        prev_grad_norm = norm(prev_gradient)
+        print(
+            f"iteration: {k:.3f}    x:, {x_prev[0]:.3f}    y: {x_prev[1]:.3f}    f(x,y): {f(x_prev[0], x_prev[1]):.5f}")
+
+        if prev_grad_norm <= epsilon:
+            return f(x_prev[0], x_prev[1]), k
+
+        if k == 0:
+            step = find_step(x_prev, calculate_step_f)
+            x_prev = x_k
+            x_k = x_prev - step * prev_gradient
+
+            k += 1
+            continue
+
+        step = find_step(x_prev, calculate_step_f)
+        x_prev = x_k
+        x_k = x_prev - step * prev_gradient
+
+        k += 1
+
+
+print(conjugate_gradient(0.05))
