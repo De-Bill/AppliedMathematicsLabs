6ad853ea8cc6616c8f1ca25dc9a6804fe0928d15
iakuliaev <iakuliaev@gmail.com>
2022-04-28 13:55:57 +0300

    feat: fixed function

--- lab2/given_func.py
+++ lab2/given_func.py
@@ -1,13 +1,17 @@
 import numpy as np
 
 
-def f(x, y):   # given func
-    return x * x + 2 * y * y + np.exp(x + y)
+def f(x, y):  # given func
+    return x * x + 2 * y * y + 2 * x * y
 
 
 def df_x(x, y):  # derivative of the given func -- df/dx
-    return 2 * x + np.exp(x + y)
+    return 2 * x + 2 * y
 
 
 def df_y(x, y):  # -- df/dy
-    return 4 * y + np.exp(x + y)
+    return 4 * y + 2 * x
+
+
+def grad(x):
+    return np.array([df_x(x[0], x[1]), df_y(x[0], x[1])])
