dff1043471a4e850aa917eacdeaf9fdebc5096d5
Gvozdoslav <remrostrus2123@yandex.ru>
2022-04-28 21:07:34 +0300

    fix: add function 3D plot and change conjugate_gradient return value

--- lab2/test.py
+++ lab2/test.py
@@ -1,11 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import cm
 
 import given_func as given
 from learning_rate_variations.const import grad_desc_with_const_lr
 from learning_rate_variations.fractional import grad_desc_with_fractional_step
 from learning_rate_variations.fibonacci import grad_desc_with_fibonacci_step
 from learning_rate_variations.golden_ratio import grad_desc_with_golden_ratio_step
+from methods.conjugate_gradient import conjugate_gradient
+
+
+def plot_function(function):
+    X, Y = np.meshgrid(np.linspace(-7, 7, 100),
+                       np.linspace(-7, 7, 100))
+
+    plt.figure(figsize=(16, 10))
+
+    axis = plt.subplot(projection='3d')
+
+    zs = np.array([function(x, y) for [x, y] in zip(np.ravel(X), np.ravel(Y))])
+    Z = zs.reshape(X.shape)
+
+    axis.plot_surface(X, Y, Z, cmap=cm.coolwarm, zorder=2)
+
+    axis.set_zlim(-5, 40)
+    axis.view_init(elev=60)
+    plt.show()
+
 
 x = np.linspace(-1.8, 1.8, 50)
 y = np.linspace(-1.8, 1.8, 50)
@@ -14,7 +35,7 @@ F = given.f(X, Y)
 
 x_start, y_start = 1, 1
 precision = 0.06
-points = grad_desc_with_fractional_step(x_start, y_start, 0.2, precision, 0.1, 0.95)
+points = conjugate_gradient(x_start=x_start, y_start=y_start, epsilon=precision)
 
 ax = plt.subplots()[1]
 
@@ -23,3 +44,5 @@ ax.plot(points[0:len(points):2], points[1:len(points):2])
 ax.contour(X, Y, F, 40)
 
 plt.show()
+
+plot_function(given.f)
